5fd8f01bd176287f7869bb0eee617625474c2ac4
Prasetia <Alex.Prasetia@acer.com>
2021-08-05 22:31:41 +0800

    adding authentication function and check local ip

--- broker.js
+++ broker.js
@@ -1,10 +1,15 @@
 var mosca = require('mosca')
 var exec = require('child_process').exec
-var settings = {url:"192.168.0.185",port: 1883}
+var url = require('os').networkInterfaces().Ethernet.filter(s=>s.family=='IPv4').map(s=>s.address)[0]
+// var port = Math.floor(Math.random() * (9999 - 1000 + 1) + 1000)
+var port = 2930
+var settings = {url:url,port: port}
 var broker = new mosca.Server(settings)
 
+broker.authenticate('charger_mqtt','smartphone','password_12345',()=>{})
 broker.on('ready', ()=>{
     console.log('mqtt server is opened')
+    console.log(url+':'+port.toString())
 })
 
 broker.on('published', (packet)=>{
@@ -12,7 +17,13 @@ broker.on('published', (packet)=>{
     console.log(message)
     if (Number(message)==0){
         console.log('Charging Stop')
-        exec('shutdown +2')
-        process.exit()
+        if (process.platform =='win32'||process.platform=='win64'){
+            exec('shutdown /s')
+            setTimeout(()=>process.exit(),1000)
+        }
+        else{ 
+            exec('shutdown +2')
+            setTimeout(()=>process.exit(),1000)
+        }
     }
 })
--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 var fs = require('fs')
 var mqtt = require('mqtt')
+, host = require('os').networkInterfaces().Ethernet.filter(s=>s.family=='IPv4').map(s=>s.address)[0]
+, port = '2930';
 
-var client = mqtt.connect('mqtt://192.168.0.185:1883')
+var settings = {
+keepalive: 1000,
+protocolId: 'MQIsdp',
+protocolVersion: 3,
+clientId: 'charger_mqtt',
+username:'smartphone',
+password: 'password_12345'
+};
+
+// client connection
+var client = mqtt.connect('mqtt://'+host+':'+port, settings);
 var topic = 'Charger'
 var battery_level=0
 if (typeof (process.argv.slice(2)[0])!='undefined'){
